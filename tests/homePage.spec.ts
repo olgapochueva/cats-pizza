109134e531f0f36664827e74e26025276654bd52
Olga Pochueva <pochuevaolga@gmail.com>
2026-04-06 22:55:01 +0300

    add e2e test

--- tests/homePage.spec.ts
+++ tests/homePage.spec.ts
@@ -1,11 +1,13 @@
 import { test, expect } from '@playwright/test';
 
+//ui test
 test('Check header', async ({ page }) => {
   await page.goto('');
   const header = page.getByTestId('homePageHeader');
   await expect(header).toBeVisible();
 });
 
+//integration test
 test('Check card list items', async ({ page }) => {
   await page.goto('');
   const firstCard = page.getByTestId('catCard_0');
